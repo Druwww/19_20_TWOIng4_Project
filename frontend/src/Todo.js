039bc5ff8f03f3825adfa8b783dc16a746b7176c
Quentin M <quentin.mulliez@edu.ece.fr>
2019-11-26 13:32:03 +0100

    clean + sources

--- frontend/src/Todo.js
+++ frontend/src/Todo.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Button, Tooltip, OverlayTrigger } from 'react-bootstrap';
 import './Todo.css';
 
+// source : https://react-bootstrap.github.io/components/overlays/#popovers
+
 //To use put a the top 
 //var dataTasks = [{name : "task 1", description :"ma description ouf"}, {name : "task 2", description :"The description ouf"}];
 //and : <Todo tasks={dataTasks}></Todo>
@@ -25,21 +27,16 @@ class Todo extends React.Component {
       return (
         <div>
           {this.props.tasks.map(({ name, description }) => (
-
-          <OverlayTrigger
-              placement="right"
-              delay={{ show: 250, hide: 400 }}
-              overlay={renderTooltip({name, description})}
-            >
-              <Button className="oneTask">
-              <p className="title"><center>{name}</center></p>
-              <p className="description">{description} </p>
-              </Button>
+            <OverlayTrigger
+                placement="right"
+                delay={{ show: 250, hide: 400 }}
+                overlay={renderTooltip({name, description})}
+              >
+                <Button className="oneTask">
+                <p className="title"><center>{name}</center></p>
+                <p className="description">{description} </p>
+                </Button>
             </OverlayTrigger>
-
-
-
-            
           ))}
         </div>
       );
