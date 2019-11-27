0cbacfac15a998ec06c65b80b63aa3c1789600c5
Quentin M <quentin.mulliez@edu.ece.fr>
2019-11-27 23:02:48 +0100

    Add BDD connection

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,12 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
+const mongoose = require('mongoose');
+const config = require('./config/database.config');
+
+// on se connecte à la base de données
+mongoose.connect(config.url, { useNewUrlParser: true });
+
 var app = express();
 
 app.use(logger("dev"));
