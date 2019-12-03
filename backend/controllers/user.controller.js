de0d77af6511ad2a5e79b47d78c45d9ef707c82d
Quentin M <quentin.mulliez@edu.ece.fr>
2019-12-03 14:13:09 +0100

    ADD : User, Fidn one with body elements

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -63,3 +63,135 @@ exports.create = (req, res) => {
       });
     });
 };
+
+
+// Find a single User with a UserId
+exports.findOne = (req, res) => {
+
+  //If id is pass in request
+  if(req.body.userId){
+    User.findById(req.body.userId)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({
+          message: 'User not found with id ' + req.body.userId
+        });
+      }
+      res.send(user);
+    })
+    .catch(err => {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).send({
+          message: 'User not found with id ' + req.body.userId
+        });
+      }
+      return res.status(500).send({
+        message: 'Error retrieving user with id ' + req.body.userId
+      });
+    });
+  }else if(req.body){
+    var diffParams = {};
+
+    if(req.body.location){
+      diffParams.location = req.body.location;
+    }
+
+    if(req.body.personsInHouse){
+      diffParams.personsInHouse = req.body.personsInHouse;
+    }
+    
+    if(req.body.houseSize){
+      diffParams.houseSize = req.body.houseSize;
+    }
+
+    // res.send(diffParams);
+
+    User.find(diffParams)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({
+          message: 'User not found with thoses params ' + diffParams
+        });
+      }
+      res.send(user);
+    })
+    .catch(err => {
+      if (err.kind === 'ObjectId') {
+        return res.status(404).send({
+          message: 'User not found with thoses params ' +diffParams
+        });
+      }
+      return res.status(500).send({
+        message: 'Error retrieving user with thoses params' + diffParams
+      });
+    });
+
+
+  }
+  else{
+    return res.status(404).send({
+      message: 'No params in request'
+    });
+  }
+};
+
+// // Update a User identified by the UserId in the request
+// exports.update = (req, res) => {
+//   // Validate Request
+//   if (!req.body.firstName) {
+//     return res.status(400).send({
+//       message: 'first name can not be empty'
+//     });
+//   }
+
+//   // Find user and update it with the request body
+//   User.findByIdAndUpdate(
+//     req.params.userId,
+//     {
+//       title: req.body.firstName,
+//       content: req.body.lastName || ''
+//     },
+//     { new: true }
+//   )
+//     .then(user => {
+//       if (!user) {
+//         return res.status(404).send({
+//           message: 'User not found with id ' + req.params.userId
+//         });
+//       }
+//       res.send(user);
+//     })
+//     .catch(err => {
+//       if (err.kind === 'ObjectId') {
+//         return res.status(404).send({
+//           message: 'User not found with id ' + req.params.userId
+//         });
+//       }
+//       return res.status(500).send({
+//         message: 'Error updating user with id ' + req.params.userId
+//       });
+//     });
+// };
+
+// // Delete a User with the specified UserId in the request
+// exports.delete = (req, res) => {
+//   User.findByIdAndRemove(req.params.userId)
+//     .then(user => {
+//       if (!user) {
+//         return res.status(404).send({
+//           message: 'User not found with id ' + req.params.userId
+//         });
+//       }
+//       res.send({ message: 'User deleted successfully!' });
+//     })
+//     .catch(err => {
+//       if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+//         return res.status(404).send({
+//           message: 'User not found with id ' + req.params.userId
+//         });
+//       }
+//       return res.status(500).send({
+//         message: 'Could not delete user with id ' + req.params.userId
+//       });
+//     });
+// };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,4 +6,6 @@ var user = require('../controllers/user.controller');
 /* create  one user   */
 router.put('/', user.create);
 
+router.get('/', user.findOne);
+
 module.exports = router;
