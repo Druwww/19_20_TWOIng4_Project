c882b2c1db916bad07feb98f78eafe934823e1f6
Quentin M <quentin.mulliez@edu.ece.fr>
2019-12-08 15:39:58 +0100

    Nhien is BAE

--- frontend/src/Number.js
+++ frontend/src/Number.js
@@ -4,38 +4,48 @@ import './Number.css';
 const axios = require('axios');
 
 
-export default class Number extends React.Component {
+class Number extends React.Component {
 
     constructor(props){
         super(props);
 
+        this.state = {
+            numberSensor : 0
+        }
         
         
-        var bonsoir =  axios.get('http://localhost:3000/sensors/numberSensors')
-        .then(function (response) {
-            // handle success
-            console.log(response.data.numberSensor);
-            return response.data.numberSensor;
-        })
 
         //Normalement bonsoir doit contenir le nombre de capteur dans la BDD
         //Mais la réponse de requette arrive après...
-        console.log(bonsoir);
+        // console.log(bonsoir);
 
-        this.state = {
-            numberSensor : bonsoir
-        }
+        // this.state = {
+        //     numberSensor : bonsoir
+        // }
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3000/sensors/numberSensors')
+        .then(response => {
+            // handle success
+            console.log(response.data.numberSensor);
+
+            this.setState({numberSensor : response.data.numberSensor});
+
+            console.log(this.state.numberSensor);
+            // return response.data.numberSensor;
+        });
     }
 
     render() {
         return (
             <div>
                 <p className="simpleText"><strong>Vous avez :</strong></p>
-                <p className = "sensorNumber"> {this.state.sensorsNumber}</p>
+                <p className = "sensorNumber"> {this.state.numberSensor}</p>
                 <p className="simpleText"> <strong>utilisateurs !</strong></p>               
             </div>
         );
     }
 }
 
-// export default Number;
+export default Number;
