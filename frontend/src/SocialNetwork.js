b535636775d4ef0e82fa17ad416df95e55cd3e5a
Quentin M <quentin.mulliez@edu.ece.fr>
2019-12-09 15:05:56 +0100

    data Link V1 + disapear effect V1 W6

--- frontend/src/SocialNetwork.js
+++ frontend/src/SocialNetwork.js
@@ -1,27 +1,66 @@
 import React, { Component } from 'react';
 import { Row, Col, Toast } from 'react-bootstrap';
+const axios = require('axios');
+
 
 class SocialNetwork extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            data : [],
+            show : []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3000/measure/lastMeasure')
+        .then(response => {
+            //limit for ce moment
+            var showIni = {};
+
+            for(var x in response.data){
+                showIni[x._id] = true;
+            }
+
+            this.setState({data: response.data, show : showIni});
+        });
+    }
+
+    // const toggleShow = (_id) => this.setState({show[_id] : false});
+
     render() {
         return (
             <div>
                 <Row>
-                    <Col>
-                        <Toast show={true}>
-                            <Toast.Header>
-                                <img
-                                    src="holder.js/20x20?text=%20"
-                                    className="rounded mr-2"
-                                    alt=""
-                                />
-                                <strong className="mr-auto">Bootstrap</strong>
-                                <small>11 mins ago</small>
-                            </Toast.Header>
-                            <Toast.Body>
-                                Nouvelle notification !
-                            </Toast.Body>
-                        </Toast>
-                    </Col>
+
+                    {this.state.data.map(({ _id, creationDate, type, sensorID, value }) => (
+
+                        <Col>
+                            <Toast show={this.state.show[_id]} onClose={() =>
+                                {
+                                    var newData = this.state.show;
+                                    newData[_id] = false;
+                                    this.setState({show : newData});
+                                }
+                                }>
+                                <Toast.Header> 
+                                    <img
+                                        src="holder.js/20x20?text=%20"
+                                        className="rounded mr-2"
+                                        alt=""
+                                    />
+                                    <strong className="mr-auto">{_id}</strong>
+                                    <small>{creationDate}</small>
+                                </Toast.Header>
+                                <Toast.Body>
+                                    {type} : {value}
+                                </Toast.Body>
+                            </Toast>
+                        </Col>
+
+                    ))}
                 </Row>
             </div>
         );
